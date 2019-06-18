b5883d0fbc75bc0724360d32fec0b0d72d416d3c
Diego Francisco <deliasfrancisco@gmail.com>
2019-06-18 14:28:59 -0300

    List

---
+++ LambdaEList/LambdaEList/Extensoes/ListExtensoes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaEList
+{
+    public static class ListExtensoes<T> //precisa ser uma metodo static detro de uma classe static para poder ser acessado externamente
+    {// <T> define o que a classe é do tipo generico
+        public static void AdicionarVarios(this List<T> listaDeInteiros, params T[]itens)//criação de uma lista de inteiros e de um vetor sem tamanho definido porem com o argumento params que auto aloca uma tamanho de array
+        {    //METODO ESTATICO                      QUE VAI ATUAR NESSA LISTA       PEGANDO OS ARGUMENTOS
+            // foi feito uso do THIS que juntamente a visibilidade publica permite o acesso direto ao metodo, que também esta estatico
+            foreach(T item in itens)
+            {
+                listaDeInteiros.Add(item);
+            }
+        }
+        // uma lista generica abrange uma grande quantidade de itens (string, char, int, double)
+    }
+}
---
+++ LambdaEList/LambdaEList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaEList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> idades = new List<int>(); //CRIAÇÃO DE UMA LISTA DE INTEIROS
+
+            idades.Add(27);//
+            idades.Add(12);//
+            idades.Add(42);//  PREECHIMENTO DE UMA LISTA DE INTEIROS
+            idades.Add(70);//
+            idades.Add(5);//
+
+            ListExtensoes.AdicionarVarios(idades, 1245, 1145, 55452);  //USO DA CLASSE ListExtensoes CRIADA COM UMA METODO DE LISTA PARA A ADIÇÃO DE VARIOS 
+            //OBS: FOI FEITO USO DO PARAMS PARA NÃO HAVER A PREOCUPAÇÃO COM O TANTO DE PARAMETROS QUE SERÃO PASSADOS 
+
+            idades.AdicionarVarios(45, 23, 41); //idade pode acessar o metodo da classe ListExtensoes diretamente pois foi feito apontado através do this que a lista esta acessivel diretamente para a classe que quiser acessalá diretamente
+
+            for (int i = 0; i < idades.Count; i++)
+            {
+                Console.WriteLine(idades[i]); //LEITURA DA LIST A DE IDADES
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
