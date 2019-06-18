8e0b9b3ca435c185f340e07e37b9c3e6c5ba1f68
Diego Francisco <deliasfrancisco@gmail.com>
2019-06-18 14:26:45 -0300

    LambdaEArrays

---
+++ csharppt8-aula3/ByteBank.SistemaAgencia/Comparadores/ComparadorContaCorrentePorAgencia.cs
@@ -0,0 +1,49 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia.Comparadores
+{
+    public class ComparadorContaCorrentePorAgencia : IComparer<ContaCorrente> //inteface Icomparer
+    {
+        // se x for igual a y retornar 
+        // se x e y forem equivalentes retornar zero 
+        // se y tiver uma precendencia maior  que x retornar positivo
+        public int Compare(ContaCorrente x, ContaCorrente y)
+        {
+            return x.Agencia.CompareTo(x.Agencia);
+                                                      
+            if (x == y)                         //     <..
+            {                                    //       `.
+                return 0;                          //       `.    
+            }                                        //       )
+                                                       //     |
+            if (x == null)                               //   |
+            {                                              // |
+                return 1;        //                           |
+            }                      //                         |  O CODIGO FEITO JÁ É IMPLEMENTADO PELO .NET
+                                     //                       | 
+            if (y == null)             //                     |
+            {                            //                   |
+                return -1;                 //                |
+            }                                //             /
+                                               //          /
+            if (x.Agencia < y.Agencia)           //      .´
+            {                                      //  .´
+                return -1; //x fica na fernte y    //-´
+            }
+            if (x.Agencia == y.Agencia)
+            {
+                return 0; //são equivalentes
+            }
+            return 1; //y fica na frente de x 
+
+            // As comparações de numeros inteiros é equivalente ao que ja existe no tipo int
+            // ctrl + k + C comenta um bloco de texto
+            // ctrl + k + U descomenta um bloco de texto
+        }
+    }
+}
---
+++ csharppt8-aula3/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using ByteBank.SistemaAgencia.Comparadores;
+using ByteBank.SistemaAgencia.Extensoes;
+
+
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var contas = new List<ContaCorrente>()
+            {
+                new ContaCorrente(341, 57480),
+                new ContaCorrente(342, 45678),
+                new ContaCorrente(340, 48950),
+                new ContaCorrente(290, 18950)
+            };
+
+            //contas.Sort(); ~~> chamar a implementação de IComparer
+
+            contas.Sort(new ComparadorContaCorrentePorAgencia());
+
+            // 1º IEnumerable<ContaCorrente> contasNaoNulas = 
+            //    contas.Where(conta => conta != null); //fazendo a validação, pegar a conta onde a conta for diferente de nulo (com o operador where)
+
+            /* 2º  IOrderedEnumerable<ContaCorrente>*/var contasOrdenadas = contas //uso do var para simplificação
+                .Where(conta => conta != null) // utilzando o orderby e o where na mesma chamada, pois ele fazia a implementação do IEnumerable no 1º para chamar aqui, agora ele faz tudo no 2º
+                .OrderBy(conta => conta.Numero);
+
+            //if (conta == null)
+            //{
+            //    return int.MinValue; // retorna o menor valor possivel para que não haja uma conta de maior valor
+            //}
+
+            //return conta.Numero;     -   Dentro do OrderBy
+
+
+            //ordenação: Esta sendo dito que vou acessar o objeto conta e que a ordenação sera pelo numero da conta selecionada
+            // designação de uma expressão lambda
+            foreach (var conta in contasOrdenadas)
+            {
+                if (conta != null)
+                {// aqui seta sendo eliminado as contas nulas
+                    Console.WriteLine($"Conta número {conta.Numero}, ag. {conta.Agencia}");
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+
+        static void TestaSort()
+        {
+            var nomes = new List<string>()
+            {
+                "Diego",
+                "Guilherme",
+                "Luana",
+                "Ana"
+            };
+
+            nomes.Sort();
+
+            foreach (var nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+
+            var idades = new List<int>();
+
+            idades.Add(1);
+            idades.Add(5);
+            idades.Add(14);
+            idades.Add(25);
+            idades.Add(38);
+            idades.Add(61);
+
+            idades.AdicionarVarios(45, 89, 12);
+            // ListExtensoes.AdicionarVarios(idades, 45, 89, 12);
+
+            idades.AdicionarVarios(99, -1);
+
+
+            idades.Sort();
+
+            for (int i = 0; i < idades.Count; i++)
+            {
+                Console.WriteLine(idades[i]);
+            }
+        }
+
+        static void TestaListaDeObject()
+        {
+            ListaDeObject listaDeIdades = new ListaDeObject();
+
+            listaDeIdades.Adicionar(10);
+            listaDeIdades.Adicionar(5);
+            listaDeIdades.Adicionar(4);
+            listaDeIdades.Adicionar("um texto qualquer");
+            listaDeIdades.AdicionarVarios(16, 23, 60);
+
+            for (int i = 0; i < listaDeIdades.Tamanho; i++)
+            {
+                int idade = (int)listaDeIdades[i];
+                Console.WriteLine($"Idade no indice {i}: {idade}");
+            }
+        }
+
+        static int SomarVarios(params int[] numeros)
+        {
+            int acumulador = 0;
+            foreach(int numero in numeros)
+            {
+                acumulador += numero;
+            }
+            return acumulador;
+        }
+
+
+        static void TestaListaDeContaCorrente()
+        {
+            //ListaDeContaCorrente lista = new ListaDeContaCorrente();
+            ListaDeContaCorrente lista = new ListaDeContaCorrente();
+
+            ContaCorrente contaDoGui = new ContaCorrente(11111, 1111111);
+
+            ContaCorrente[] contas = new ContaCorrente[]
+            {
+                contaDoGui,
+                new ContaCorrente(874, 5679787),
+                new ContaCorrente(874, 5679754)
+            };
+
+            lista.AdicionarVarios(contas);
+
+            lista.AdicionarVarios(
+                contaDoGui,
+                new ContaCorrente(874, 5679787),
+                new ContaCorrente(874, 5679787),
+                new ContaCorrente(874, 5679787),
+                new ContaCorrente(874, 5679787),
+                new ContaCorrente(874, 5679787),
+                new ContaCorrente(874, 5679787),
+                new ContaCorrente(874, 5679787),
+                new ContaCorrente(874, 5679787)
+            );
+
+            for (int i = 0; i < lista.Tamanho; i++)
+            {
+                ContaCorrente itemAtual = lista[i];
+                Console.WriteLine($"Item na posição {i} = Conta {itemAtual.Numero}/{itemAtual.Agencia}");
+            }
+        }
+
+
+
+        static void TestaArrayDeContaCorrente()
+        {
+            ContaCorrente[] contas = new ContaCorrente[]
+                {
+                    new ContaCorrente(874, 5679787),
+                    new ContaCorrente(874, 4456668),
+                    new ContaCorrente(874, 7781438)
+                };
+
+            for (int indice = 0; indice < contas.Length; indice++)
+            {
+                ContaCorrente contaAtual = contas[indice];
+                Console.WriteLine($"Conta {indice} {contaAtual.Numero}");
+            }
+        }
+
+        static void TestaArrayInt()
+        {
+            // ARRAY de inteiros, com 5 posições!
+            int[] idades = null;
+            idades = new int[3];
+
+            idades[0] = 15;
+            idades[1] = 28;
+            idades[2] = 35;
+            //idades[3] = 50;
+            //idades[4] = 28;
+            //idades[5] = 60;
+
+            Console.WriteLine(idades.Length);
+
+            int acumulador = 0;
+            for (int indice = 0; indice < idades.Length; indice++)
+            {
+                int idade = idades[indice];
+
+                Console.WriteLine($"Acessando o array idades no índice {indice}");
+                Console.WriteLine($"Valor de idades[{indice}] = {idade}");
+
+                acumulador += idade;
+            }
+
+            int media = acumulador / idades.Length;
+            Console.WriteLine($"Média de idades = {media}");
+        }
+
+    }
+}
